f9ece42c5824f0c09e1f395ff48ed4c48fc6d875
Anna Bryzgalova <annabryzgalova1984@gmail.com>
2022-10-02 18:43:06 +0300

    Диплом на проверку вариант 1

---
+++ public/homePage.js
@@ -0,0 +1,108 @@
+//1. Выход из личного кабинета 
+const logoutButtonOne = new LogoutButton();
+logoutButtonOne.action = () => {
+    ApiConnector.logout((response) => {
+        if (response.success) {
+            location.reload();
+        }
+    });
+};
+
+//2.Получение информации о пользователе 
+ApiConnector.current((response) => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+    }
+});
+
+//3.Получение текущих курсов валюты 
+const ratesBoardOne = new RatesBoard();
+const period = () => {
+    ApiConnector.getStocks(response => {
+        if (response.success) {
+            ratesBoardOne.clearTable();
+            ratesBoardOne.fillTable(response.data);
+        }
+    })
+}
+period();
+setInterval(period, 60000);
+
+
+//4. Операции с деньгами
+//4.1 Пополнение баланса 
+const moneyManagerOne = new MoneyManager();
+moneyManagerOne.addMoneyCallback = (data) => {
+        ApiConnector.addMoney(data, (response) => {
+            if (response.success) {
+                ProfileWidget.showProfile(response.data);
+                moneyManagerOne.setMessage(response.success, 'Баланс пополнен');
+            } else {
+                moneyManagerOne.setMessage(response.success, response.error);
+            }
+        })
+    }
+    //4.2 конвертирование валюты
+moneyManagerOne.conversionMoneyCallback = (data) => {
+    ApiConnector.convertMoney(data, (response) => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManagerOne.setMessage(response.success, 'Конвертирование выполнено');
+        } else {
+            moneyManagerOne.setMessage(response.success, response.error);
+        }
+    })
+};
+
+//4.3 перевод валюты
+moneyManagerOne.sendMoneyCallback = (data) => {
+    ApiConnector.transferMoney(data, (response) => {
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            moneyManagerOne.setMessage(response.success, 'Перевод выполнен');
+        } else {
+            moneyManagerOne.setMessage(response.success, response.error);
+        }
+    })
+};
+
+
+//5. Работа с избранным
+const FavoritesWidgetOne = new FavoritesWidget();
+
+//5.1 Запрос списка избранного
+ApiConnector.getFavorites(response => {
+    if (response.success) {
+        FavoritesWidgetOne.clearTable();
+        FavoritesWidgetOne.fillTable(response.data);
+        moneyManagerOne.updateUsersList(response.data);
+    }
+});
+
+//5.2 Добавление пользователя в список избранных
+FavoritesWidgetOne.addUserCallback = (data) => {
+    ApiConnector.addUserToFavorites(data, (response) => {
+        if (response.success) {
+            FavoritesWidgetOne.clearTable();
+            FavoritesWidgetOne.fillTable(response.data);
+            moneyManagerOne.updateUsersList(response.data);
+            FavoritesWidgetOne.setMessage(response.success, 'Пользователь добавлен в список избранных');
+        } else {
+            FavoritesWidgetOne.setMessage(response.success, response.error);
+        }
+    })
+}
+
+//5.3 Удаление пользователя из списка избранных
+FavoritesWidgetOne.removeUserCallback = (data) => {
+    ApiConnector.removeUserFromFavorites(data, (response) => {
+        if (response.success) {
+            FavoritesWidgetOne.clearTable();
+            FavoritesWidgetOne.fillTable(response.data);
+            moneyManagerOne.updateUsersList(response.data);
+            FavoritesWidgetOne.setMessage(response.success, 'Пользователь удален из списка избранных');
+        } else {
+            FavoritesWidgetOne.setMessage(response.success, response.error);
+        }
+    })
+}
